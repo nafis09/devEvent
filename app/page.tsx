7e2bb237cae3eb405ce840e451e1abf8c2d59f40
nafis09 <nafisahmed003@gmail.com>
2026-03-27 20:57:15 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups and Conferences, All in One Place.</p>
+        </section>
+    )
+}
+export default Page

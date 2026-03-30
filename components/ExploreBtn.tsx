c840ae797c6c1b7e034688e8b914f627ce83c1d6
nafis09 <nafisahmed003@gmail.com>
2026-03-30 13:39:01 +0600

    feat: added homepage components

---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import Image from "next/image";
+
+const ExploreBtn = () => {
+    return (
+        <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log('clicked')}>
+            <a href="#events">
+                Explore Events
+                <Image src="/icons/arrow-down.svg" alt="arrow-down" width={20} height={20} />
+            </a>
+        </button>
+    )
+}
+export default ExploreBtn

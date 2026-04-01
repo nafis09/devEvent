332eea2fb6828ed5a3859ff8d111449ea5a1951d
nafis09 <nafisahmed003@gmail.com>
2026-04-01 13:20:59 +0600

    feat:add event details page and submit

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import {notFound} from "next/navigation";
 import Image from "next/image";
 import { cacheLife } from "next/cache";
+import BookEvent from "@/components/BookEvent";
 
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -96,6 +97,8 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> 
     const agendaItems = normalizeStringArray(agenda);
     const tagItems = normalizeStringArray(tags);
 
+    const bookings = 10;
+
     return (
         <section id="event">
            <div className="header">
@@ -134,7 +137,18 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> 
                 </div>
 
                 <aside className="booking">
-                    <p className="text-lg font-semibold">Book Event</p>
+                    <div className="signup-card">
+                        <h2>Book Your Spot</h2>
+                        {bookings > 0 ? (
+                            <p className="text-sm">
+                                Join {bookings} people who have already booked their spot!
+                            </p>
+                        ): (
+                            <p className="text-sm">Be the first to book your spot!</p>
+                        )}
+
+                        <BookEvent />
+                    </div>
                 </aside>
             </div>
         </section>
